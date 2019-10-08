050adc20108ce29fee6b6bb563d5ee33e2bbefe9
vladyslavvorobei <vladyslavvorobei@gmai.com>
2019-10-08 12:45:16 +0300

    add modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import Swiper from 'swiper';
 import slick from 'slick-slider';
 import '../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min'
+import '../../node_modules/jquery-popup-overlay/jquery.popupoverlay';
+import '../../node_modules/jquery-mask-plugin/dist/jquery.mask.min';
+import '../../node_modules/jquery-validation/dist/jquery.validate.min';
 
 $(function() {
 // BURGER MOBILE MENU
@@ -435,4 +438,83 @@ $(function() {
       }
     }
   });
+
+
+  // BUTTON UP
+  $(document).scroll(function() {
+    // - - - - SCROOL - - - - OPEN
+    if($(this).scrollTop() > 900) {
+      $('.button-up').removeClass('button-up--hidden');
+    }
+    else {
+      $('.button-up').addClass('button-up--hidden');
+    }
+    // - - - - SCROLL - - - - CLOSE
+  });
+  $('.button-up').on('click', function(event) {
+    const $target = $(this.getAttribute('href'));
+
+    if ($target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: $target.offset().top - 140
+      }, 1000);
+    }
+  });
+
+  // MODAL
+  $('.modal').popup({
+    transition: 'all 0.3s',
+    outline: true,
+    focusdelay: 400,
+    vertical: 'top'
+  });
+
+
+  $('input[type="tel"]').mask('+7 (000) 000-00-00');
+
+  jQuery.validator.addMethod('phoneno', function (phoneNumber, element) {
+    return this.optional(element) || phoneNumber.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+  }, 'Введите Ваш телефон');
+
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: 'POST',
+      url: 'sendmail.php',
+      data,
+      success() {
+        $('.modal').popup('hide');
+        $('#thanks').popup('show');
+        setTimeout(function () {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
+
+  $('.form').each(function (index, el) {
+    $(el).addClass(`form-${index}`);
+
+    $(`.form-${index}`).validate({
+      rules: {
+        name: 'required',
+        message: 'required',
+        phone: {
+          required: true,
+          phoneno: true
+        }
+      },
+      messages: {
+        name: 'Введите Ваше имя',
+        phone: 'Введите Ваш телефон',
+        message: 'Оставьте Ваше сообщение'
+      },
+      submitHandler(form) {
+        const t = $(`.form-${index}`).serialize();
+        ajaxSend(`.form-${index}`, t);
+      }
+    });
+  });
+
 });
